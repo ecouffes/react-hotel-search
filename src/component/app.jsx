2156d7ec90a0c89b59416da4d1bf07390432ce6f
K-Tanaka (aka Écouffes) <tanaka-katsunori@dmm.com>
2018-05-13 23:16:37 +0900

    Greeting

---
+++ src/component/app.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-indent,react/jsx-indent-props */
+import React, { Component } from 'react';
+import Greeting from './greeting';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'John',
+        };
+    }
+
+    handleNameChange(name) {
+        this.setState({ name });
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={e => this.handleNameChange(e.target.value)}
+                />
+                <Greeting name={this.state.name} />
+                <button onClick={() => this.handleNameChange('Bob')}>I am Bob</button>
+            </div>
+        );
+    }
+}
+
+export default App;
